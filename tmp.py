f312b354c3692faf928be4adf88e88d111d28e89
Hao Fu <haofu@ucdavis.edu>
2017-04-13 19:56:50 -0700

    Modify file paths

--- tmp.py
+++ tmp.py
@@ -1,61 +1,22 @@
-from utils import Utilities
+import fileinput
 import os
-from sensitive_component import SensitiveComponent
 
 
-class SimpleDataFormatter:
-    TAG = 'DataFormatter'
-    logger = Utilities.set_logger(TAG)
-    gnd_dir = None
+def parse_labelled(gnd_dir, instances):
+    for root, dirs, files in os.walk(gnd_dir):
+        for file_name in files:
+            if file_name.endswith('.md'):
+                # try:
 
-    @staticmethod
-    def handle_md(md_file, instances):
-        counter = len(instances)
-        SimpleDataFormatter.logger.info(md_file)
-        with open(md_file, 'r') as text_file:
-            for line in text_file:
-                sub_lines = line.split('|')
-                 
-                if ('D' or 'd') in sub_lines[len(sub_lines) - 2]:
-                    label = 'labelled_D'
-                    print(line)
-                else:
-                    continue
-                if line.startswith('#'):
-                    continue
-                SimpleDataFormatter.logger.info(sub_lines)
-                xml_path = sub_lines[len(sub_lines) - 4].split('(')[1]
-                xml_path = xml_path.split('.png')[0] + '.xml'
-                SimpleDataFormatter.logger.info(xml_path + ', ' + line)
-                doc = []
-
-                SimpleDataFormatter.logger.info(doc)
-
-                entry_name = sub_lines[len(sub_lines) - 5].split(';')[0]
-                entry_sub_words = SensitiveComponent.SensEntryPoint.split_entry_name(entry_name)
-                doc.append(entry_sub_words)
-                instances[counter] = {}
-                instances[counter]['doc'] = doc
-                instances[counter]['label'] = label
-                instances[counter]['entry_name'] = entry_name
-                instances[counter]['xml_path'] = xml_path
-                counter += 1
-
-    @staticmethod
-    def parse_labelled(gnd_dir, instances):
-        SimpleDataFormatter.gnd_dir = gnd_dir
-        for root, dirs, files in os.walk(gnd_dir):
-            for file_name in files:
-                if file_name.endswith('.md'):
-                    # try:
-                    SimpleDataFormatter.handle_md(os.path.join(root, file_name), instances)
-                    # except Exception as e:
-                    #   DataFormatter.logger.error(e)
+                file = os.path.join(root, file_name)
+                with fileinput.FileInput(file, inplace=True) as file:
+                    for line in file:
+                        print(line.replace('D:\\', 'F:\\'), end='')
 
 
 if __name__ == '__main__':
     instances = {}
-    gnd_based_dir = 'output/drebin/gnd/'
+    gnd_based_dir = 'output/gnd/'
     perm_type = 'Location'
     instance_dir = gnd_based_dir + '/' + perm_type
-    SimpleDataFormatter.parse_labelled(instance_dir, instances)
+    parse_labelled(instance_dir, instances)
