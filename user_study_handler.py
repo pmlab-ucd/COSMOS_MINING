73a831221e614290b880cab9a6c012a8a8458f4c
Hao Fu <haofu@ucdavis.edu>
2017-04-29 20:58:32 -0700

    User study

--- user_study_handler.py
+++ user_study_handler.py
@@ -6,81 +6,88 @@ from data_formatter import DataFormatter
 import csv
 
 pngs = []
-
-'''
-To handle user study feedback
-'''
+examined = []
 
 
 class UserStudyHandler:
-    TAG = 'DataFormatter'
+    """
+    Handle users' study feedback
+    """
+    TAG = 'UserStudyHandler'
     logger = Utilities.set_logger(TAG)
     gnd_dir = None
 
     @staticmethod
-    def handle_md(md_file, headers, instances):
+    def parse_md(md_file, headers, instances):
         counter = len(instances)
         UserStudyHandler.logger.info(md_file)
         with open(md_file, 'r') as text_file:
             for line in text_file:
                 sub_lines = line.split('|')
 
-                #if not (('T' or 't' or 'F' or 'f' or 'D' or 'd') in sub_lines[len(sub_lines) - 2]):
-                 #   continue
-
                 if line.startswith('#') or line.startswith('| Index ') or line.startswith('| --'):
                     continue
-                UserStudyHandler.logger.info(line)
+                #UserStudyHandler.logger.info(line)
                 png_path = sub_lines[len(sub_lines) - 4].split('(')[1]
                 png_path = png_path.split(')')[0]
 
-                '''
-                if ('D' or 'd') in sub_lines[len(sub_lines) - 2]:
-                    label = 'labelled_D'
-                    print(line)
-                else:
-                    continue
-                '''
-
                 found = False
+                click = False
                 for header in headers:
                     act = str(header)
-                    act.replace('- Copy', '')
+                    if '- Copy' in act:
+                        act = act.replace('- Copy', '')
+                        click = True
                     if act in png_path:
-                        UserStudyHandler.logger.info(header)
+                        #UserStudyHandler.logger.info(header)
                         found = True
                         break
                 if not found or line in md_targets:
                     continue
-                md_targets.append(line)
+
 
                 if png_path not in pngs:
                     pngs.append(png_path)
                 xml_path = png_path.split('.png')[0] + '.xml'
                 UserStudyHandler.logger.info(xml_path + ', ' + line)
-                doc = []
-                DataFormatter.handle_dynamic_xml(xml_path, doc)
-                UserStudyHandler.logger.info(doc)
+                texts = []
+                DataFormatter.handle_dynamic_xml(xml_path, texts)
+                #UserStudyHandler.logger.info(texts)
 
                 entry_name = sub_lines[len(sub_lines) - 5].split(';')[0]
-                entry_sub_words = SensitiveComponent.SensEntryPoint.split_entry_name(entry_name)
-                doc.append(entry_sub_words)
+                if 'Click' in entry_name and not click:
+                    continue
+                class_name, method_name = SensitiveComponent.SensEntryPoint.extract_class_method(entry_name)
+                if class_name in examined:
+                    continue
+                else:
+                    examined.append(class_name)
+                md_targets.append(line)
+                UserStudyHandler.logger.info(class_name)
+                class_name, method_name = SensitiveComponent.SensEntryPoint.split_entry_name(entry_name)
+
+                texts.append(class_name)
+                texts.append(method_name)
                 instance = {}
-                instance['doc'] = doc
+                instance['texts'] = texts
                 instance['entry_name'] = entry_name
                 instance['xml_path'] = xml_path
                 instance['png_path'] = png_path
                 instances.append(instance)
                 counter += 1
 
+
     @staticmethod
-    def parse_labelled(gnd_dir, headers, instances):
+    def parse_mds(gnd_dir, headers, instances):
+        """
+        Given elements extracted from CSV, search all instances matched with the elements in all markdown files 
+        """
         UserStudyHandler.gnd_dir = gnd_dir
         for root, dirs, files in os.walk(gnd_dir):
             for file_name in files:
                 if file_name.endswith('.md'):
                     # try:
-                    UserStudyHandler.handle_md(os.path.join(root, file_name), headers, instances)
+                    UserStudyHandler.parse_md(os.path.join(root, file_name), headers, instances)
                     # except Exception as e:
                     #   DataFormatter.logger.error(e)
 
@@ -89,18 +96,34 @@ class UserStudyHandler:
         f = open(csv_path)
         reader = csv.reader(f)
         headers = next(reader, None)
-        #columns = {}
-        #for h in headers:
-         #   columns[h] = []
+
         users = []
         for row in reader:
             user = dict()
             for h, v in zip(headers, row):
                 user[h] = v
-                #columns[h].append(v)
+                # columns[h].append(v)
             users.append(user)
-        #print(columns)
+        # print(columns)
         print(users)
+
+
+        columns = {}
+
+        for h in headers:
+            columns[h] = []
+
+        for row in reader:
+            for h, v in zip(headers, row):
+                columns[h].append(v)
+        print(len(columns))
+
+        headers = []
+        for h in columns:
+            if len(str(h)) == 0:
+                continue
+            headers.append(h)
+
         return headers, users
 
 
@@ -112,7 +135,8 @@ if __name__ == '__main__':
     perm_type = 'users'
     instance_dir = gnd_based_dir + '/' + perm_type + '/'
     headers, users = UserStudyHandler.read_user_csv(instance_dir + 'Location.csv')
-    UserStudyHandler.parse_labelled(gnd_based_dir + '/Location/', headers, instances)
+    print(len(headers), len(users))
+    UserStudyHandler.parse_mds(gnd_based_dir + '/Location/', headers, instances)
     # print(len(pngs))
     print(len(md_targets))
     md_file_path = instance_dir + 'Location.md'
@@ -145,7 +169,7 @@ if __name__ == '__main__':
         outdir = instance_dir + '/' + str(users.index(user)) + '/'
         if os.path.exists(outdir):
             shutil.rmtree(outdir)
-        DataFormatter.instances2txtdocs(outdir, new_instances)
+        DataFormatter.instances2txtdocs(outdir, new_instances, who=False)
 
     '''
     for png in pngs:
